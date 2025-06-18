c050268367afcca18a0bbbc7078fbcb4977638ec
yokke <it.yteshim@gmail.com>
2025-06-18 21:11:55 +0900

    toppageスタイル変更

--- frontend/src/components/Layout/UserLayout/SideNav.vue
+++ frontend/src/components/Layout/UserLayout/SideNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="user-sidenav">
+  <nav class="side-nav">
     <ul>
       <li><a href="#">ホーム</a></li>
       <li><a href="#">プロフィール</a></li>
@@ -8,25 +8,6 @@
   </nav>
 </template>
 
-<script setup>
-// 必要があれば props や emits を追加
-</script>
-
-<style scoped>
-.user-sidenav {
-  width: 200px;
-  background-color: #f4f4f4;
-  padding: 1rem;
-}
-.user-sidenav ul {
-  list-style: none;
-  padding: 0;
-}
-.user-sidenav li {
-  margin-bottom: 0.5rem;
-}
-.user-sidenav a {
-  text-decoration: none;
-  color: #333;
-}
+<style lang="scss">
+@import '@/assets/User/main.scss';
 </style>
